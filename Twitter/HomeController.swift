c4b8482f40f845570e9c125ef973560471fe0e2d
Trang Le <tranglt@tmh-techlab.vn>
2017-07-06 17:21:21 +0700

    Init

---
+++ Twitter/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  Twitter
+//
+//  Created by TrangLe on 7/5/17.
+//  Copyright © 2017 TrangLe. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .blue
+    }
+}
---
+++ Twitter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by TrangLe on 7/5/17.
+//  Copyright © 2017 TrangLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
